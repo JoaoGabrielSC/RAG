295f90d70711ddb0da486d7d4228a8475b1c0450
Joao <joaogabriel.sc99@gmail.com>
2025-07-19 03:31:59 -0300

    adding

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -1,15 +1,20 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 
 class Parser:
-    def __init__(self):
+    def __init__(self) -> None:
         self.parser = ArgumentParser(description="Parser for command line arguments")
         self.add_arguments()
 
-    def add_arguments(self):
+    def add_arguments(self) -> None:
         self.parser.add_argument(
             "--dataset_path", type=str, required=True, help="Path for dataset files"
         )
 
-    def parse(self):
-        return self.parser.parse_args()
+    def parse(self, args: list[str] | None = None) -> Namespace:
+        """
+        Here you can pass a list of arguments to parse.
+        """
+        parse_namespace = self.parser.parse_args(args)
+
+        return parse_namespace
